180446de0b6415fdf1e218eeacab5c4927c92ecf
Andy <andyhall1983@hotmail.co.uk>
2022-08-09 20:14:13 +0100

    Fill out logic in playRound function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,38 @@
 function getComputerChoice ()
 {
     let computerChoice = ['rock', 'paper', 'scissors'];
-    let randomElement = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    console.log(randomElement);
+    let randomChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    return randomChoice;
 }
 
 function playRound (playerSelection, computerSelection)
 {
-      
+    if (playerSelection == computerSelection)
+    {
+        console.log('its a tie!');
+    }
+    else if 
+    (
+        playerSelection == 'rock' && computerSelection == 'paper' || 
+        playerSelection == 'scissors' && computerSelection == 'rock' || 
+        playerSelection == 'paper' && computerSelection == 'scissors' 
+    )
+    {
+        console.log ('you lose');
+    }
+    else if
+    (
+        playerSelection == 'rock' && computerSelection == 'scissors' || 
+        playerSelection == 'scissors' && computerSelection == 'paper' || 
+        playerSelection == 'paper' && computerSelection == 'rock' 
+    )
+    {
+        console.log ('you win');
+    }
+    else 
+    {
+        console.log ('error')
+    }  
 }
 
 const playerSelection = window.prompt ('enter; rock, paper, or scissors');
