12cd2b7bf93e2f790fdb0cf6f6268ad81adf4759
Andy <andyhall1983@hotmail.co.uk>
2022-08-12 06:33:51 +0100

    Add lines to output computer and playerSelection variables to playRound function, experiment with game function

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ function playRound (getPlayerChoice, getComputerChoice)
 
     if (playerSelection == computerSelection)
     {
+        console.log('computer chose: ' + (computerSelection) + ', you chose: ' + (playerSelection));
         console.log('its a tie this time');
     }
     else if 
@@ -33,6 +34,7 @@ function playRound (getPlayerChoice, getComputerChoice)
         playerSelection == 'paper' && computerSelection == 'scissors' 
     )
     {
+        console.log('computer chose: ' + (computerSelection) + ', you chose: ' + (playerSelection));
         console.log ('you lose this one', + computerScore+1);
         return computerScore+1;
     }
@@ -43,6 +45,7 @@ function playRound (getPlayerChoice, getComputerChoice)
         playerSelection == 'paper' && computerSelection == 'rock' 
     )
     {
+        console.log('computer chose: ' + (computerSelection) + ', you chose: ' + (playerSelection));
         console.log ('you win this one', + playerScore+1);
         return playerScore+1;
     }
@@ -52,7 +55,28 @@ function playRound (getPlayerChoice, getComputerChoice)
     }  
 }  
 
-playRound();
+let scores = playRound();
+
+function game (playRound, playerScore, computerScore)
+{
+    playRound();
+    while (scores < 6)
+    {
+        if (playerScore == 5)
+        {
+            break;
+            console.log('you are the winner')
+        }
+        else if (computerScore == 5)
+        {
+            console.log('the computer won')
+            break;
+        }
+        else scores++;      
+    }
+
+    game();
+}
 
 
 
