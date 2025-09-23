b718278b80f48a1df264b5671c7271429c240bc9
sungazing8 <andyhall1983@hotmail.co.uk>
2025-09-23 06:51:44 +0100

    Change compareChoices function name to playRound, complete the function logic

--- javascript.js
+++ javascript.js
@@ -16,13 +16,29 @@ function getHumanChoice(){
     return (userChoice);
 }
 
-function compareChoices(){
+function playRound(){
     const finalComputerChoice = getComputerChoice(3);
     console.log(finalComputerChoice);
     const finalUserChoice = getHumanChoice();
     console.log(finalUserChoice);
 
-  
+    if(finalUserChoice === "rock" && finalComputerChoice === "scissors"){
+        console.log ("rock beats scissors, you win this round!"); 
+    }  else if (finalUserChoice === "paper" && finalComputerChoice === "rock"){
+        console.log("paper beats rock, you win this round");
+    }  else if (finalUserChoice === "scissors" && finalComputerChoice === "paper"){
+        console.log("scissors beats paper, you win this round");
+    }  else if (finalUserChoice === "rock" && finalComputerChoice === "paper"){
+        console.log("paper beats rock, you lose this round");
+    }  else if (finalUserChoice === "paper" && finalComputerChoice === "scissors"){
+        console.log("scissors beats paper, you lose this round");
+    }  else if (finalUserChoice === "scissors" && finalComputerChoice === "rock"){
+        console.log("rock beats scissors, you lose this round");
+    }  else {
+        console.log("this ones a draw, go again");
+    }
+      
+    
 }    
 
-compareChoices();
+playRound();
