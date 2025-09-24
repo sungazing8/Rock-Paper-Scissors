a752d46ec59b43f5eb8246bb844c4eb8ff67e926
sungazing8 <andyhall1983@hotmail.co.uk>
2025-09-24 07:09:39 +0100

    Refactor playRound function to show points update in log

--- javascript.js
+++ javascript.js
@@ -19,34 +19,34 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanScore, computerScore){
+function playRound(){
     const finalComputerChoice = getComputerChoice(3);
     const finalUserChoice = getHumanChoice();
 
     if(finalUserChoice === "rock" && finalComputerChoice === "scissors"){
-        
-        console.log ("rock beats scissors, you win this round!"); 
+        console.log ("rock beats scissors, you win this round!" + " you have " + humanScore +1 + " points"); 
+        return (humanScore + 1); 
+
+    }  else if (finalUserChoice === "paper" && finalComputerChoice === "rock"){ 
+        console.log("paper beats rock, you win this round!" + " you have " + humanScore +1 + " points");
         return (humanScore + 1);
-    }  else if (finalUserChoice === "paper" && finalComputerChoice === "rock"){
-        
-        console.log("paper beats rock, you win this round");
-        return (humanScore + 1);
+
     }  else if (finalUserChoice === "scissors" && finalComputerChoice === "paper"){
-        
-        console.log("scissors beats paper, you win this round");
+        console.log("scissors beats paper, you win this round" + " you have " + humanScore +1 + " points");
         return (humanScore + 1);
+
     }  else if (finalUserChoice === "rock" && finalComputerChoice === "paper"){
-        
-        console.log("paper beats rock, you lose this round");
+        console.log("paper beats rock, you lose this round" + " computer has " + computerScore +1 + " points");
         return (computerScore + 1);
+
     }  else if (finalUserChoice === "paper" && finalComputerChoice === "scissors"){
-        
-        console.log("scissors beats paper, you lose this round");
+        console.log("scissors beats paper, you lose this round" + " computer has " + computerScore +1 + " points");
         return (computerScore + 1);
+
     }  else if (finalUserChoice === "scissors" && finalComputerChoice === "rock"){
-        
-        console.log("rock beats scissors, you lose this round");
+        console.log("rock beats scissors, you lose this round" + " computer has " + computerScore +1 + " points");
         return (computerScore + 1);
+
     }  else {
         console.log("this ones a draw, go again");
     }
